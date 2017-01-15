df8e363a0d8e57ea284faeabd1a9c8b8b6df568a
Ryan Johnson <ryan@johnsonweb.us>
2017-01-14 16:44:10 -0800

    More vision work

--- camera-calibration/compile.sh
+++ camera-calibration/compile.sh
@@ -1,1 +1,1 @@
-g++ camera_calibration.cp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_features2d -lopencv_calib3d
+g++ camera_calibration.cp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_features2d -lopencv_calib3d -lusb-1.0 -I/usr/local/include/libusb-1.0 
