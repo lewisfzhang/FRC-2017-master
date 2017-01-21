72c6cddef28a6bfafb068cffd7f25b5c49d907e4
0xFireball <0xFireball@outlook.com>
2017-01-21 13:46:16 -0800

    add pixy

---
+++ src/com/team254/frc2017/pixy/Frame.java
@@ -0,0 +1,45 @@
+package com.team254.frc2017.pixy;
+
+import java.util.List;
+
+public class Frame {
+	
+	public static class Block {
+		public static enum SyncType {
+			NORMAL, COLOR_CODE
+		}
+		SyncType sync;
+		int checksum, signature, centerX, centerY, width, height;
+		@Override
+		public String toString() {
+			return ":Block { signature: "+signature+", center: ("+centerX+", "+centerY+"), size: "+width+"x"+height+" }";
+		}
+	}
+	
+	Frame(List<Frame.Block> blocks, int id) {
+		this.blocks = blocks;
+		this.id = id;
+	}
+	
+	public List<Frame.Block> getBlocks() {
+		return blocks;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "Frame{ blockCount: "+blocks.size()+", id: "+ id;
+		for (Frame.Block b : blocks) {
+			str += "\n    "+b;
+		}
+		str += "\n}";
+		return str;
+	}
+	
+	protected List<Frame.Block> blocks;
+	protected int id;
+	
+}
