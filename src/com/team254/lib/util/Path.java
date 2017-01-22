e904c5a4e8579e4ce716ba75525bc041085c8730
SuperMario210 <Sup3rMar1o210@gmail.com>
2017-01-21 20:59:32 -0800

    added look ahead to path class

--- src/com/team254/lib/util/Path.java
+++ src/com/team254/lib/util/Path.java
@@ -1,15 +1,17 @@
 package com.team254.lib.util;
 
 import java.io.File;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import com.team254.frc2017.Constants;
 
 public class Path {
      List<PathSegment> segments;
      
      public Path(String filepath) {
-         segments = new LinkedList<PathSegment>();
+         segments = new ArrayList<PathSegment>();
          loadFile(filepath);
      }
      
@@ -36,6 +38,41 @@ public class Path {
          }
      }
      
+     public Translation2d getTargetPoint(Translation2d robotPos) {
+         Translation2d target;
+         PathSegment.Segment currentSegment = (PathSegment.Segment) segments.get(0);
+         double segmentLength = currentSegment.getLength();
+         double lookAheadDist = Constants.kAutoLookAhead;
+         Translation2d closest = currentSegment.getClosestPoint(robotPos);
+         double remainingDist = currentSegment.getRemainingDistance(closest);
+         if(lookAheadDist - remainingDist <= 0) {
+             target = currentSegment.getLookAheadPoint(segmentLength + lookAheadDist - remainingDist);
+         } else {
+             lookAheadDist -= remainingDist;
+             int i = 1;
+             while(lookAheadDist > 0 && i < segments.size()) {
+//                 if(segments.get(i).isTurn()) {
+//                     return ((PathSegment.Turn)segments.get(i)).getCenter();
+//                 }
+                 currentSegment = (PathSegment.Segment)segments.get(i);
+                 lookAheadDist -= currentSegment.getLength();
+                 i++;
+             }
+             target = currentSegment.getLookAheadPoint(currentSegment.getLength() + lookAheadDist);
+         }
+         checkSegmentDone(robotPos);
+         return target;
+     }
+     
+     public void checkSegmentDone(Translation2d robotPos) {
+         PathSegment.Segment currentSegment = (PathSegment.Segment) segments.get(0);
+         double length = currentSegment.getLength();
+         double remainingDist = currentSegment.getRemainingDistance(currentSegment.getClosestPoint(robotPos));
+         if((length - remainingDist) / length > Constants.kAutoSegmentThreshold) {
+             segments.remove(0);
+         }
+     }
+     
      public static void main(String[] args) {
 //         PathSegment.Segment curve = new PathSegment.Segment(10.0, 0.0, 0.0, 10.0, 0.0, 0.0, 0.1, 0, 3.0 * Math.PI/2, 1.0);
 //         Translation2d p = new Translation2d(1.0, 1.0);
@@ -47,9 +84,10 @@ public class Path {
 //         System.out.println(line.getClosestPoint(p));
 //         System.out.println(line.getLength());
          
-         
-         
-         //Path test = new Path("~/path.txt");
+         //PathSegment.Segment curve = new PathSegment.Segment(43.533040796611594, 200.4199794406889, 100, 250.0, 100, 193.05472389242087, 100);
+         //System.out.println(curve.getLookAheadPoint(18.05));
+         Path test = new Path("~/path.txt");
+         System.out.println(test.getTargetPoint(new Translation2d(20, 20)));
      }
      
 }
