d6c88f41da19b8771b5fcd7aa4fbe0261d64b0a9
Solomon Chang <changsolomon8@gmail.com>
2017-03-06 13:53:16 -0800

    Fix minor formatting

--- src/com/team254/frc2017/Robot.java
+++ src/com/team254/frc2017/Robot.java
@@ -217,7 +217,7 @@ public class Robot extends IterativeRobot {
                     mSuperstructure.setShooterOpenLoop(8.0);
                 } else if (mControlBoard.getShooterClosedLoopButton()) {
                     mSuperstructure.setClosedLoopRpm(3000.0);
-                } else if (!mControlBoard.getHangButton()){
+                } else if (!mControlBoard.getHangButton()) {
                     mSuperstructure.setShooterOpenLoop(0);
                 }
             }
