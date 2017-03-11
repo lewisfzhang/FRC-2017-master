75a768c29903068ec4738094a87155398f5d0904
Solomon Chang <changsolomon8@gmail.com>
2017-03-11 13:09:25 -0800

    Add ability to blink LED

--- src/com/team254/frc2017/subsystems/LED.java
+++ src/com/team254/frc2017/subsystems/LED.java
@@ -6,6 +6,9 @@ import com.team254.frc2017.loops.Looper;
 import edu.wpi.first.wpilibj.Relay;
 
 public class LED extends Subsystem {
+    private static final int kDefaultBlinkCount = 3;
+    private static final double kBlinkDuration = 0.1; // seconds for full cycle
+    private static final double kTotalBlinkDuration = kDefaultBlinkCount * kBlinkDuration;
 
     private static LED mInstance = null;
 
@@ -18,11 +21,11 @@ public class LED extends Subsystem {
 
     // Internal state of the system
     public enum SystemState {
-        OFF, FIXED_ON
+        OFF, FIXED_ON, BLINKING
     }
 
     public enum WantedState {
-        OFF, FIXED_ON
+        OFF, FIXED_ON, BLINK
     }
 
     private SystemState mSystemState = SystemState.OFF;
@@ -54,9 +57,13 @@ public class LED extends Subsystem {
         public void onLoop(double timestamp) {
             synchronized (LED.this) {
                 SystemState newState;
+                double timeInState = timestamp - mCurrentStateStartTime;
                 switch (mSystemState) {
                     case OFF:
                         newState = handleOff();
+                        break;
+                    case BLINKING:
+                        newState = handleBlinking(timeInState);
                         break;
                     case FIXED_ON:
                         newState = handleFixedOn();
@@ -83,6 +90,8 @@ public class LED extends Subsystem {
         switch (mWantedState) {
             case OFF:
                 return SystemState.OFF;
+            case BLINK:
+                return SystemState.BLINKING;
             case FIXED_ON:
                 return SystemState.FIXED_ON;
             default:
@@ -98,6 +107,23 @@ public class LED extends Subsystem {
     private synchronized SystemState handleFixedOn() {
         setLEDOn();
         return defaultStateTransfer();
+    }
+
+    private synchronized SystemState handleBlinking(double timeInState) {
+        if (timeInState > kTotalBlinkDuration) {
+            setLEDOff();
+            // Transition to OFF state and clear wanted state.
+            mWantedState = WantedState.OFF;
+            return SystemState.OFF;
+        }
+
+        int cycleNum = (int)(timeInState / (kBlinkDuration / 2.0));
+        if ((cycleNum % 2) == 0) {
+            setLEDOn();
+        } else {
+            setLEDOff();
+        }
+        return SystemState.BLINKING;
     }
 
 
