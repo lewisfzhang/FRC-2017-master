61793e35c9ab7fad971be2d2fa118a5c684fd751
Leigh Pauls <leighpauls@fb.com>
2017-03-11 19:37:01 -0800

    update plotting scripts

--- csv_logging/plot_data.py
+++ csv_logging/plot_data.py
@@ -13,16 +13,7 @@ data = np.genfromtxt('SHOOTER-LOGS.csv', delimiter=',', names=True)
 
 fig = plt.figure()
 
-outputPlot = fig.add_subplot(3, 1, 1)
-outputPlot.plot(data['time'], data['newOutput'], label='output')
-outputPlot.set_ylim([7, 9])
-
-errorPlot = fig.add_subplot(3, 1, 2)
-errorPlot.plot(data['time'], data['error'], label='error')
-errorPlot.set_ylim([-200, 200])
-
-meanSquareErrorPlot = fig.add_subplot(3, 1, 3)
-meanSquareErrorPlot.plot(data['time'], data['meanSquareError'], label='mean square error')
-meanSquareErrorPlot.set_ylim([0, 600])
+outputPlot = fig.add_subplot(1, 1, 1)
+outputPlot.plot(data['time'], data['flywheel_rpm'], label='flywheel_rpm')
 
 plt.show()
