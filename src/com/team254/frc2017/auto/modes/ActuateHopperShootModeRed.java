67b78c0e6230ae2346b7ad59a368904cc1e82709
Solomon Chang <changsolomon8@gmail.com>
2017-03-18 10:58:04 -0700

    updated auto modes

--- src/com/team254/frc2017/auto/modes/ActuateHopperShootModeRed.java
+++ src/com/team254/frc2017/auto/modes/ActuateHopperShootModeRed.java
@@ -26,21 +26,36 @@ public class ActuateHopperShootModeRed extends AutoModeBase {
     protected void routine() throws AutoModeEndedException {
         PathContainer hopperPath = new BoilerToHopperRed();
         runAction(new ResetPoseFromPathAction(hopperPath));
-        
+
         runAction(
                 new ParallelAction(Arrays.asList(new Action[]{
-                    new SetFlywheelRPMAction(3000.0), //spin up flywheel to save time
-                    new DrivePathAction(hopperPath), //drive to hopper
-                    new SeriesAction(Arrays.asList(new Action[]{
-                            //deploy intake early to save time (need to test to see if it messes with the path)
-                            new WaitForPathMarkerAction("DeployIntake"), new DeployIntakeAction(true)
-                    })),
-                    new SeriesAction(Arrays.asList(new Action[]{
-                            //actuate hopper and begin shooting once path finishes
-                            new WaitForPathMarkerAction("PathFinished"), new ActuateHopperAction(true), new WaitAction(0.5), new BeginShootingAction(), new WaitAction(4), new LowerGearAction()
-                    }))
+                        new SetFlywheelRPMAction(3000.0), //spin up flywheel to save time
+                        new DrivePathAction(hopperPath) //drive to hopper
                 }))
         );
+
+        runAction(new DeployIntakeAction());
+        runAction(new WaitAction(1.0));
+        runAction(new ActuateHopperAction(true));
+        runAction(new WaitAction(0.5));
+        runAction(new BeginShootingAction());
+        runAction(new WaitAction(4));
+        runAction(new LowerGearAction());
+
+//        runAction(
+//                new ParallelAction(Arrays.asList(new Action[]{
+//                    new SetFlywheelRPMAction(3000.0), //spin up flywheel to save time
+//                    new DrivePathAction(hopperPath), //drive to hopper
+//                    new SeriesAction(Arrays.asList(new Action[]{
+//                            //deploy intake early to save time (need to test to see if it messes with the path)
+//                            new WaitForPathMarkerAction("DeployIntake"), new DeployIntakeAction(true)
+//                    })),
+//                    new SeriesAction(Arrays.asList(new Action[]{
+//                            //actuate hopper and begin shooting once path finishes
+//                            new WaitForPathMarkerAction("PathFinished"), new ActuateHopperAction(true), new WaitAction(0.5), new BeginShootingAction(), new WaitAction(4), new LowerGearAction()
+//                    }))
+//                }))
+//        );
         runAction(new WaitAction(15)); //keep shooting until auto ends
     }
 }
