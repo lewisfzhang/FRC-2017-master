3722257352c4bf53b34051dd3bbc85ef49c90fd6
Solomon Chang <changsolomon8@gmail.com>
2017-03-27 14:28:14 -0700

    Change hostname for script

--- csv_logging/fetch_and_plot.sh
+++ csv_logging/fetch_and_plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp admin@roborio-252-frc.local:/home/lvuser/SHOOTER-LOGS.csv . && python plot_data.py 
+scp admin@roborio-254-frc.local:/home/lvuser/SHOOTER-LOGS.csv . && python plot_data.py
 
