a3dd2fbf0a52b618dc0346ba64467d76c3371632
Leigh Pauls <leighpauls@fb.com>
2017-03-30 19:35:16 -0700

    simplified firstperson view stream

--- dash/firstperson.js
+++ dash/firstperson.js
@@ -5,24 +5,9 @@ var webcam;
 function startStream() {
   webcam = document.getElementById("webcam");
 
-  setInterval(nextImage, 20);
+  setInterval(renewStream, 2000);
 }
 
-
-function nextImage() {
-  var img = new Image();
-  img.style.position = "absolute";
-  img.style.zIndex = -1;
-  img.style.width = "100%";
-  img.style.height = "100%";
-  var imageZIndexClosure = imageNr;
-  img.onload = function() {
-    this.style.zIndex = imageZIndexClosure;
-    while (this.previousElementSibling != null) {
-      this.parentElement.removeChild(this.previousElementSibling);
-    }
-  };
-  img.src = "http://roborio-254-frc.local:5801/?action=snapshot&n=" + (++imageNr);
-  webcam.appendChild(img);
-
+function renewStream() {
+  webcam.src = "http://roborio-254-frc.local:5801/?action=stream&n=" + (++imageNr);
 }
