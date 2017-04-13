84f72c2ce4d60a8624acee647ba6b9134488638b
Solomon Chang <changsolomon8@gmail.com>
2017-04-13 14:30:18 -0700

    Make shooting during tuning actually work.

--- src/com/team254/frc2017/Robot.java
+++ src/com/team254/frc2017/Robot.java
@@ -183,7 +183,12 @@ public class Robot extends IterativeRobot {
             double turn = mControlBoard.getTurn();
 
             if (mControlBoard.getAimButton()) {
-                mDrive.setWantDriveTowardsGoal();
+
+                if (Constants.kIsShooterTuning) {
+                    mDrive.setWantAimToGoal();
+                } else {
+                    mDrive.setWantDriveTowardsGoal();
+                }
 
                 if (mControlBoard.getUnjamButton()) {
                     mSuperstructure.setWantedState(Superstructure.WantedState.UNJAM_SHOOT);
